683f74845e21282ecb97730c4b6e7bf5f93b6798
Skylar <syema.ailia@gmail.com>
2018-09-06 10:36:54 -0500

    aritmetic types

--- September6.java
+++ September6.java
@@ -4,20 +4,23 @@ class September6
 {
    public static void main( String[] args )
    {
+      
       Scanner input = new Scanner(System.in);
       
+      // set empty int variable 
       int sum1;
       int sum2;
+      
+      //get two int inputs to set to int variable
       System.out.println("Please enter a number to be added: ");
-      
       sum1 = input.nextInt();
-      
       System.out.println("Please enter another number to be added: ");
-      
       sum2 = input.nextInt();
       
+      //calculate total
       int total = sum1 + sum2;
       
+      //output total
       System.out.println("The total is " + total);
       
    
@@ -33,18 +36,34 @@ class September6_
    {
       Scanner input = new Scanner(System.in);
       
-      
+      /*
       //get two int inputs 
-      System.out.println("Please enter a number to be added: ");
+      System.out.println("Enter an integer: ");
       int first = input.nextInt();
-      System.out.println("Please enter another number to be added: ");
+      System.out.println("Enter an integer: ");
       int second = input.nextInt();
       
       //calculate total:
       int total = first + second;
       
       //output total
-      System.out.println("The total is " + total);
+      System.out.println("The sum is " + total);
+      */
+      
+      int num1 = 19;
+      int num2 = 7;
+      
+      int sum = num1 + num2;
+      int difference = num1 - num2;
+      int product = num1 * num2;
+      int remainder = num1 % num2;
+      
+      System.out.println( "The sum is " + sum );
+      System.out.println( "The difference is " + difference );
+      System.out.println( "The product is " + product );
+      System.out.println( "The remainder is " + remainder );
+      
+      
    }
    
 }
