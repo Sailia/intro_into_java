2268592a0dbdadc8a4dd550f5d0825d3a2f98593
Skylar <syema.ailia@gmail.com>
2018-09-13 09:48:35 -0500

    if gpa

---
+++ Selections.java
@@ -0,0 +1,22 @@
+//selection structures: like stairways and hallways
+public class Selections
+{
+   public static void main( String[] args)
+   {
+      double gpa = 2.88;
+      
+      if(gpa >= 2.88)
+      {
+         System.out.println("Great Job Syema!");
+         System.out.println("Keep up the good work!");
+      }
+      
+      if(gpa < 2.88)
+      {
+         System.out.println("Work harder!");
+         System.out.println("You will get there!");
+      }
+   
+   }
+
+} 
