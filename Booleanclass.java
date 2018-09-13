a0d0815e8e769832adf60de1849ae1e0e3e8d372
Skylar <syema.ailia@gmail.com>
2018-09-13 09:43:35 -0500

    more boolean stuff

---
+++ Booleanclass.java
@@ -0,0 +1,13 @@
+public class Booleanclass
+{ 
+   public static void main(String[] args)
+   {
+   
+      boolean r1 = (5 < 10) && (5 > 2) && (5 !=0 );
+      boolean r2 = (5 < 2) && (5 > 2) || (5 !=0 );
+      
+      System.out.println(r1);
+      System.out.println(r2);
+   }
+   
+}
