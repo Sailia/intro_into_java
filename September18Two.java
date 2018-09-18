886d91f8b9628accd7ee0ea4e2d421bc9ad10878
Skylar <syema.ailia@gmail.com>
2018-09-18 10:40:12 -0500

    file arrangement

---
+++ September18Two.java
@@ -0,0 +1,20 @@
+public class September18Two
+{
+   public static void main( String[] args )
+   {
+      int number = 13;
+      
+      if(number < 5 )
+      {
+         System.out.println("Number is less than 5");
+      }
+      else if( number > 2)
+      {
+         System.out.println("Number is greater than 22");
+      }
+      else
+      {
+         System.out.println("Number is neither");
+      }
+   }
+}
