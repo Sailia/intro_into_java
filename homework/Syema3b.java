65a6c7712bce9eec0f5bca675a1721f92b884774
Skylar <syema.ailia@gmail.com>
2018-09-19 21:52:49 -0500

    reverse number without string manipulation

---
+++ homework/Syema3b.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+/*
+Part 3b:Create a Java class named YourName3b so mine would be 
+Carden3b.java
+Prompt the user for a four digit integer.  
+This program will calculate and display the reverse of the four digit 
+integer.  For example if the user entered the integer 3621, 
+the program will calculate and display 1263.  
+You may assume that the user will always enter a four digit integer 
+(verification not required).  
+Hint:  you do not need loops for this program.  
+Think about how to solve this using integer division 
+and the modulus operator.  Note: you may not use Strings to 
+solve this problem.
+*/
+
+public class Syema3b
+{
+   public static void main( String[] args )
+   {
+    Scanner input = new Scanner(System.in);
+    
+    System.out.println("Please enter a four digit integer: ");
+    int number = input.nextInt(); 
+    
+    int first_digit = (number / 1) % 10;
+    int second_digit = (number / 10) % 10;
+    int third_digit = (number / 100) % 10;
+    int last_digit = (number / 1000) % 10;
+    
+    System.out.println(number + " reversed is: " + first_digit + "" + second_digit + "" + third_digit + "" + last_digit);   
+   }
+
+}
