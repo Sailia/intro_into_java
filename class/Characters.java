b35c21083397ff2b6df2218b7e9f65abadf6f410
Skylar <syema.ailia@gmail.com>
2018-10-02 10:28:02 -0500

    char

---
+++ class/Characters.java
@@ -0,0 +1,11 @@
+public class Characters
+{
+   public static void main( String[] args )
+   {
+      char letter = 'x';
+      char space = ' ';
+      char number = '7';
+      
+      System.out.println(letter);
+   }
+}
