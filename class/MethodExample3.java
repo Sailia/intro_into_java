29ccc9751e65e2ed90232ffe2bb861ee44333ec0
Skylar <syema.ailia@gmail.com>
2018-10-11 10:41:16 -0500

    method examples

---
+++ class/MethodExample3.java
@@ -0,0 +1,15 @@
+public class MethodExample3
+{
+   public static void main( String[] args )
+   {
+      
+      System.out.println("the average is " + avg(5,8));
+   }
+   
+   public static double avg(int a, int b)
+   {
+      double average = (a + b) / 2;
+      return average;
+   }
+
+}
