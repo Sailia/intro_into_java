38eff21dce497b4043f3e01f35fb46bd60cd7a36
Skylar <syema.ailia@gmail.com>
2018-10-21 18:32:23 -0500

    input the product of two random numbers

---
+++ Syema5c.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Syema5c
+{
+   public static void main( String[] args )
+   {
+      int rand1 = randomNumber();
+      int rand2 = randomNumber();
+      int product = rand1 * rand2;
+      int response;
+      Scanner input = new Scanner(System.in);
+      
+      System.out.println("Welcome to the multiplication game!");
+      System.out.println("What is " + rand1 + " * " + rand2 + "?");
+      
+      response = input.nextInt();
+      
+      while ( response != product )
+      {
+         System.out.println(response + " is not correct, please try again");
+         System.out.println("What is " + rand1 + " * " + rand2 + "?");
+         response = input.nextInt();
+      }
+      System.out.println(response + " is correct!");
+      
+   }
+   
+   public static int randomNumber()
+   {
+      int random_num = (int) (Math.random() * 11) + 1;
+      return random_num;
+   }
+   
+}
