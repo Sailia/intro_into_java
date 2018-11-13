4a3d9948d4eeb7de5f420f953e26ef62e8caf4f4
Skylar <syema.ailia@gmail.com>
2018-11-13 09:49:58 -0600

    problem copyarrays

---
+++ CopyArrays.java
@@ -0,0 +1,16 @@
+public class CopyArrays  {
+   public static void main(String[] args) {
+      
+   }
+   
+   public static int[] copyArray(int[])   {
+      int[] copy = new int[x.length];
+      
+      for(int index = 0; index < x.length; index++;)  {
+         copy[index] = x[index];
+      }
+      return copy;
+   }
+   //when passing an array to a method, the reference of the array is passed to the method. this is called pass by reference
+   // that means any changes done to that array in the method will reflect the original array 
+}
