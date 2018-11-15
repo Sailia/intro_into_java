d95e72d664492e79c1e06234dc0051c53e2aa70f
Skylar <syema.ailia@gmail.com>
2018-11-15 10:15:14 -0600

    nested loop 2

---
+++ NestedLoop3.java
@@ -0,0 +1,13 @@
+public class NestedLoop3  {
+   public static void main(String[] args) {
+      for(int i= 1; i <= 3; i++) {
+         for(int j = 1; j <= 5; j++)   {
+            if( i < j)
+               System.out.print("X ");
+            else
+               System.out.print("O ");
+         }
+         System.out.println();
+      }
+   }
+}
