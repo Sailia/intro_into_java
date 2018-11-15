cd10dee3de29fcefc12e477da32b16d5094d456e
Skylar <syema.ailia@gmail.com>
2018-11-15 09:51:36 -0600

    nested loop 1

---
+++ NestedLoop.java
@@ -0,0 +1,14 @@
+public class NestedLoop  {
+   public static void main(String[] args) {
+      for(int i= 1; i <= 3; i++) {
+         int j = 1;
+         while(j <= 2)  {
+            System.out.print((i * j) + " ");
+            j++;
+         }
+         System.out.println();
+      }
+   }
+   
+
+}
