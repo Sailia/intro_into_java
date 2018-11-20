26e8fffc240e373d640d604d7a628769b791ba58
Skylar <syema.ailia@gmail.com>
2018-11-20 10:27:44 -0600

    2D arrays

---
+++ November20.java
@@ -0,0 +1,14 @@
+public class November20 {
+   public static void main(String[] args) {
+      int[][] table = new int[3][3];
+   }
+   
+   public static void print2DArray(int[][] array)  {
+      for(int outer = 0; outer < array.length; outer++)  {
+         for(int inner = 0; inner < array[outer].length; inner++) {
+            System.out.print(array[outer][inner] + " ");
+         }
+         System.out.println();
+      }
+   }  
+}
