f6f7d6e43ae2c739b350b7097d9e41e1fe4e2ad0
Skylar <syema.ailia@gmail.com>
2018-12-06 10:24:55 -0600

    review hw 6c

---
+++ HwReview6c.java
@@ -0,0 +1,22 @@
+public class HwReview6c   {
+   public static void main(String[] args) {
+
+   }
+   
+   public static int evenOrOdd(int[] A)   {
+      int evenSum = 0;
+         int oddProduct = 1;
+         int diff;
+         
+         for(int i = 0; i < A.length; i++)   {
+            if(i % 2 == 0)   {
+               evenSum += A[i];
+            }
+            else  {
+               oddProduct *= A[i];
+            }
+         }
+         diff = evenSum - oddProduct;
+         return diff;
+   }
+}
